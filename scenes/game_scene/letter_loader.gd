91b79112b6f6e2e458f38a14688585010331f31c
noahmacaulay <usernameneeded@gmail.com>
2026-01-31 19:32:13 -0400

    Basic text loader

--- scenes/game_scene/letter_loader.gd
+++ scenes/game_scene/letter_loader.gd
@@ -1,13 +1,32 @@
 extends Node
 
-@export var starting_index = 1
-
+@export var starting_index = 0
+var current_index = starting_index
+@export var level_folder_name = "level1"
+var letter_strings: Array[String] = []
 
 func _ready() -> void:
-	if (starting_index == 1):
-		%LetterText.set_letter_text(test1)
+	print("ASDASDSADASDA")
+	var path:String = "res://resources/level_letters/"+level_folder_name
+	
+	var dir = DirAccess.open(path)
+	if dir:
+		print("Opened folder+ "+path)
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		var file
+		while file_name != "":
+			file = FileAccess.open(path+"/"+file_name, FileAccess.READ)
+			letter_strings.append(file.get_as_text())
+			print("found file name: "+file_name)
+			file_name = dir.get_next()
+		var a = 1
 	else:
-		%LetterText.set_letter_text(test2)
+		return
+	
+	
+	%LetterText.set_letter_text(letter_strings[current_index])
+
 
 func evaluate_text() -> Array[int]:
 	return %LetterText.update_censoring(true)
